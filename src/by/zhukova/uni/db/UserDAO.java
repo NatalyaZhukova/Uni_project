e61a9ab4355b412a1fd6e15fad42b7c8a323efeb
NatalyaZhukova <alisonka1992@yandex.ru>
2015-06-23 11:46:28 +0300

    Update UserDAO.java

--- src/by/zhukova/uni/db/UserDAO.java
+++ src/by/zhukova/uni/db/UserDAO.java
@@ -15,6 +15,12 @@ import by.zhukova.uni.entity.User;
 public class UserDAO extends AbstractDAO {
 
 	static Logger logger = Logger.getLogger(UserDAO.class);
+	private final String SELECT_ALL = "SELECT * FROM users";
+	private final String SELECT_BY_ID = "SELECT * FROM users WHERE id=?";
+	private final String SELECT_BY_USERNAME = "SELECT * FROM users WHERE username=?"; // specific User method
+	private final String DELETE = "DELETE FROM users WHERE id=?";
+	private final String CREATE = "INSERT INTO users (id, username, password, user_type) VALUES (?, ?, ?, ?)";
+	private final String UPDATE = "UPDATE users SET  username=?, password=?, user_type=? WHERE id=?";
 
 	public UserDAO(Connection connection) {
 		super(connection);
@@ -24,9 +30,8 @@ public class UserDAO extends AbstractDAO {
 	@Override
 	public List<User> findAll() {
 		List<User> list = new ArrayList<User>();
-		String query = "SELECT * FROM users";
 		try {
-			PreparedStatement pst = connection.prepareStatement(query);
+			PreparedStatement pst = connection.prepareStatement(SELECT_ALL);
 			ResultSet res = pst.executeQuery();
 			while (res.next()) {
 				User user = new User();
@@ -37,21 +42,23 @@ public class UserDAO extends AbstractDAO {
 
 				list.add(user);
 			}
-			super.close(pst);
+			
 
 		} catch (SQLException e) {
 			logger.error(e);
+		} finally {
+			close(pst);
 		}
 		return list;
 	}
 
 	@Override
 	public User findEntityById(int id) {
-		String query = "SELECT * FROM users WHERE id=?";
+		
 		PreparedStatement pst;
 		User user = null;
 		try {
-			pst = connection.prepareStatement(query);
+			pst = connection.prepareStatement(SELECT_BY_ID);
 			pst.setInt(1, id);
 			ResultSet res = pst.executeQuery();
 			while (res.next()) {
@@ -61,9 +68,36 @@ public class UserDAO extends AbstractDAO {
 				user.setPassword(res.getString(3));
 				user.setUserType(res.getString(4));
 			}
-			super.close(pst);
+			
 		} catch (SQLException e) {
 			logger.error(e);
+		} finally {
+			close(pst);
+		}
+
+		return user;
+	}
+	
+	public User findUserByUsername(String username) {
+		
+		PreparedStatement pst;
+		User user = null;
+		try {
+			pst = connection.prepareStatement(SELECT_BY_USERNAME);
+			pst.setString(1, username);
+			ResultSet res = pst.executeQuery();
+			while (res.next()) {
+				user = new User();
+				user.setId(res.getInt(1));
+				user.setUsername(res.getString(2));
+				user.setPassword(res.getString(3));
+				user.setUserType(res.getString(4));
+			}
+			
+		} catch (SQLException e) {
+			logger.error(e);
+		} finally {
+			close(pst);
 		}
 
 		return user;
@@ -71,21 +105,22 @@ public class UserDAO extends AbstractDAO {
 
 	@Override
 	public boolean delete(int id) {
-		String query = "DELETE FROM users WHERE id=?";
 		PreparedStatement pst;
 		boolean result;
 		try {
-			pst = connection.prepareStatement(query);
+			pst = connection.prepareStatement(DELETE);
 			pst.setInt(1, id);
 			int check = pst.executeUpdate();
 			if (check == 0) {
 				result = false;
 			}
 			result = true;
-			super.close(pst);
+			
 		} catch (SQLException e) {
 			logger.error(e);
 			result = false;
+		} finally {
+			close(pst);
 		}
 		return result;
 	}
@@ -94,20 +129,21 @@ public class UserDAO extends AbstractDAO {
 	public boolean delete(Entity entity) {
 		boolean result;
 		int id = entity.getId();
-		String query = "DELETE FROM users WHERE id=?";
 		PreparedStatement pst;
 		try {
-			pst = connection.prepareStatement(query);
+			pst = connection.prepareStatement(DELETE);
 			pst.setInt(1, id);
 			int check = pst.executeUpdate();
 			if (check == 0) {
 				result = false;
 			}
 			result = true;
-			super.close(pst);
+			
 		} catch (SQLException e) {
 			logger.error(e);
 			result = false;
+		} finally {
+			close(pst);
 		}
 		return result;
 	}
@@ -115,11 +151,10 @@ public class UserDAO extends AbstractDAO {
 	@Override
 	public boolean create(Entity entity) {
 		boolean result;
-		String query = "INSERT INTO users (id, username, password, user_type) VALUES (?, ?, ?, ?)";
 		PreparedStatement pst;
 		User user = (User) entity;
 		try {
-			pst = connection.prepareStatement(query);
+			pst = connection.prepareStatement(CREATE);
 			pst.setInt(1, user.getId());
 			pst.setString(2, user.getUsername());
 			pst.setString(3, user.getPassword());
@@ -129,10 +164,12 @@ public class UserDAO extends AbstractDAO {
 				result = false;
 			}
 			result = true;
-			super.close(pst);
+			
 		} catch (SQLException e) {
 			logger.error(e);
 			result = false;
+		} finally {
+			close(pst);	
 		}
 		return result;
 	}
@@ -141,10 +178,9 @@ public class UserDAO extends AbstractDAO {
 	public boolean update(Entity entity) {
 		boolean result;
 		User user = (User) entity;
-		String query = "UPDATE users SET  username=?, password=?, user_type=? WHERE id=?";
 		PreparedStatement pst;
 		try {
-			pst = connection.prepareStatement(query);
+			pst = connection.prepareStatement(UPDATE);
 			pst.setString(1, user.getUsername());
 			pst.setString(2, user.getPassword());
 			pst.setString(3, user.getUserType());
@@ -154,11 +190,13 @@ public class UserDAO extends AbstractDAO {
 				result = false;
 			}
 			result = true;
-			super.close(pst);
+			
 
 		} catch (SQLException e) {
 			logger.error(e);
 			result = false;
+		} finally {
+			close(pst);
 		}
 		return result;
 	}
